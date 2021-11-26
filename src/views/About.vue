04859adfa2b68573efc7ab813ebbcd5d901829d1
张燕良 <zhangyanliang@kfang.com>
2021-11-26 14:23:03 +0800

    动画

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="btn-box">
+      <div :class="item.class" v-for="(item, index) in listBtn" :key="item">
+        {{ item.leable }} {{ index + 1 }}
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      listBtn: [
+        {
+          leable: '按钮',
+          class: ['btn-animation-light'],
+        },
+        {
+          leable: '按钮',
+          class: ['btn-animation-bottom-left-up'],
+        },
+        {
+          leable: '按钮',
+          class: ['btn-animation-downward-middle-circle'],
+        },
+      ]
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.btn-box {
+  display: flex;
+  padding: 50px;
+}
+</style>
